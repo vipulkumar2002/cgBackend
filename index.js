a50ee9867783221cc1d513176a0104b5e5d3c786
vipulkumar2002 <vipulkumarpatel2002@gmail.com>
2022-03-17 01:16:16 +0530

    gitignore file added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("App is runnig fine");
+  res.send("App is runnig.....");
 });
 
 app.listen(4000);
